a72b22f2ef794f74d0867f19cf6006e67c781bf7
Syamil Maulod <syamil8@gmail.com>
2024-03-30 09:06:45 +0100

    removed unused method

--- handlers/ministry.go
+++ handlers/ministry.go
@@ -1,12 +1,10 @@
 package handlers
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"gahmen-api/helpers"
-	"gahmen-api/types"
 )
 
 func (h *Handler) ListMinistry(w http.ResponseWriter, r *http.Request) error {
@@ -15,20 +13,6 @@ func (h *Handler) ListMinistry(w http.ResponseWriter, r *http.Request) error {
 		return err
 	}
 	return helpers.WriteJSON(w, http.StatusOK, ministries)
-}
-
-func (h *Handler) CreateMinistry(w http.ResponseWriter, r *http.Request) error {
-	createAccountRequest := new(types.CreateMinistryRequest)
-	if err := json.NewDecoder(r.Body).Decode(createAccountRequest); err != nil {
-		return err
-	}
-
-	ministry := types.NewMinistry(createAccountRequest.Name)
-
-	if err := h.store.CreateMinistry(ministry); err != nil {
-		return err
-	}
-	return helpers.WriteJSON(w, http.StatusCreated, ministry)
 }
 
 func (h *Handler) GetMinistryByID(w http.ResponseWriter, r *http.Request) error {
