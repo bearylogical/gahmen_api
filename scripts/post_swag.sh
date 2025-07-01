8a666d769abbf6a396101399ff9b081b72d72089
Syamil Maulod <syamil8@gmail.com>
2025-07-01 20:37:24 +0800

    cross compatible post_swag.sh

--- scripts/post_swag.sh
+++ scripts/post_swag.sh
@@ -2,11 +2,20 @@
 
 DOCS_FILE="./docs/docs.go"
 
+# Detect OS and set sed in-place flag
+if sed --version >/dev/null 2>&1; then
+    # GNU sed (Linux)
+    SED_INPLACE=(-i)
+else
+    # BSD sed (macOS)
+    SED_INPLACE=(-i '')
+fi
+
 # Add os import
-sed -i '' 's#import "github.com/swaggo/swag"#import (\n\t"github.com/swaggo/swag"\n\t"os"\n)#g' $DOCS_FILE
+sed "${SED_INPLACE[@]}" 's#import "github.com/swaggo/swag"#import (\n\t"github.com/swaggo/swag"\n\t"os"\n)#g' "$DOCS_FILE"
 
 # Set Host dynamically
-sed -i '' 's#Host:             ""#Host:             os.Getenv("HOST")#g' $DOCS_FILE
+sed "${SED_INPLACE[@]}" 's#Host:             ""#Host:             os.Getenv("HOST")#g' "$DOCS_FILE"
 
 # Replace ApiKeyAuth with BearerAuth in security definitions
-sed -i '' 's#"ApiKeyAuth": \[\\\]#"BearerAuth": \[\\\]#g' $DOCS_FILE
+sed "${SED_INPLACE[@]}" 's#"ApiKeyAuth": \[\\\]#"BearerAuth": \[\\\]#g' "$DOCS_FILE"
